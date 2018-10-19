ceb0b6bac5a61ac1f822793806f592297c23714c
Josh Palmer <Josh.Palmer@mendix.com>
2018-10-19 12:13:26 +0200

    Refactor Ringo to work as module and script

--- bin/ringo.py
+++ bin/ringo.py
@@ -61,37 +61,42 @@ class LogBufferFlusher:
         self.loop.close()
 
 
-# Parse command-line options
-parser = argparse.ArgumentParser(
-    description="Buffers a log stream provided as stdin until a max size or "
-    "timeout treshold has been reached, then outputs it"
-)
-parser.add_argument(
-    "--max-buffer-size",
-    type=int,
-    help="max buffer size (in bytes)",
-    default=400,
-)
-parser.add_argument(
-    "--max-storage-length",
-    type=int,
-    help="max amount of lines to store",
-    default=1000,
-)
-parser.add_argument(
-    "--interval", type=int, help="flush interval (in seconds)", default=3
-)
+def run_logger_buffer_flusher(interval, max_buffer_size, max_storage_length):
+    logger_buffer_flusher = LogBufferFlusher(
+        interval, max_buffer_size, max_storage_length
+    )
 
-args = parser.parse_args()
+    try:
+        logger_buffer_flusher.buffer_and_flush_logs()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        logger_buffer_flusher.close()
 
-# Run main operation
-logger_buffer_flusher = LogBufferFlusher(
-    args.interval, args.max_buffer_size, args.max_storage_length
-)
 
-try:
-    logger_buffer_flusher.buffer_and_flush_logs()
-except KeyboardInterrupt:
-    pass
-finally:
-    logger_buffer_flusher.close()
+if __name__ == "__main__":
+    # Parse command-line options
+    parser = argparse.ArgumentParser(
+        description="Buffers a log stream provided as stdin until a max size or "
+        "timeout treshold has been reached, then outputs it"
+    )
+    parser.add_argument(
+        "--max-buffer-size",
+        type=int,
+        help="max buffer size (in bytes)",
+        default=400,
+    )
+    parser.add_argument(
+        "--max-storage-length",
+        type=int,
+        help="max amount of lines to store",
+        default=1000,
+    )
+    parser.add_argument(
+        "--interval", type=int, help="flush interval (in seconds)", default=3
+    )
+
+    args = parser.parse_args()
+    run_logger_buffer_flusher(
+        args.interval, args.max_buffer_size, args.max_storage_length
+    )
